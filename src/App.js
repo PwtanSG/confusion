6eabee422a5d631cb28401643d1fc4b57000b7b4
pwtan <pwtan.edu@gmail.com>
2020-11-01 12:07:46 +0800

    wk1 ex1 media app class

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
+import React, { Component } from 'react'
 import { Navbar, NavbarBrand } from 'reactstrap';
 import MenuMedia from './components/MenuMediaComponent';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes'
 
-function App() {
+class App extends Component {
+
+  render(){
   return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
-        </div>
-      </Navbar>
-      <MenuMedia />
-    </div>
-  );
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">reactstrap</NavbarBrand>
+          </div>
+        </Navbar>
+        <MenuMedia />
+      </div>
+    );
+  }
 }
 
 export default App;
