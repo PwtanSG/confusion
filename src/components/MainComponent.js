5f2f02351b36377e3dc21a89f88bcd51d312c7a8
pwtan <pwtan.edu@gmail.com>
2020-11-21 11:42:40 +0800

    Wk2-Assignm2-SinglePageApp

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
@@ -63,6 +64,7 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
+          <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/>} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/> }/>
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
