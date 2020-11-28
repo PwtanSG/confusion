22a5868ce241db6aed0ed75c814b2fd1a713ca7a
pwtan <pwtan.edu@gmail.com>
2020-11-28 18:22:32 +0800

    Wk4-ex-redux-form-revisit

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import { Breadcrumb, BreadcrumbItem,Button, Row, Col, Label } from 'reactstrap';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors } from 'react-redux-form';
 
 
 //functions to form validation parameters
@@ -32,7 +32,8 @@ class Contact extends Component {
     handleSubmit(values){
         console.log('Current State is: ' + JSON.stringify(values));
         alert('Current State is: ' + JSON.stringify(values));
-        //event.preventDefault();
+        this.props.resetFeedbackForm();
+        // event.preventDefault();
 
     }
 
@@ -88,7 +89,7 @@ class Contact extends Component {
                    </div>
                    <div className="col-12 col-md-9">
                    {/*using redux form */}    
-                   <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                   <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                             <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name*</Label>
                                 <Col md={10}>
@@ -211,7 +212,7 @@ class Contact extends Component {
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                     </div>
                 </div>
             </div>
