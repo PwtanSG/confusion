13ad5db9982dcfdcf72d1465edbee1fa019e0760
pwtan <pwtan.edu@gmail.com>
2020-11-29 16:18:23 +0800

    Wk4-ex-react-animation-component

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrum
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 // import CommentForm from './CommentFormComponent';
 
 //-- functions to form validation parameters
@@ -117,14 +118,18 @@ const minLength = (len) => (val) => val && (val.length >= len);
                 <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
                     <ul className="list-unstyled">
+                        <Stagger in>
                         {comments.map((comment)=>{
                             return (
-                                <li key={comment.id}>
-                                    <p>{comment.comment}</p>
-                                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p><br />
-                                </li>         
+                                <Fade in>
+                                    <li key={comment.id}>
+                                        <p>{comment.comment}</p>
+                                        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p><br />
+                                    </li>     
+                                </Fade>    
                             );
                         })}
+                        </Stagger>
                     </ul>
                     <CommentForm dishId={dishId} postComment={postComment}/>
                 </div>
@@ -147,6 +152,10 @@ const minLength = (len) => (val) => val && (val.length >= len);
         return (    
 
         <div className="col-12 col-md-5 m-1">
+            <FadeTransform in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+            }}>
               <Card>
                 <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>	
                 <CardBody>
@@ -154,7 +163,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
                     <CardText>{dish.description}</CardText>
                 </CardBody>
               </Card>
-
+            </FadeTransform>
         </div>
         )
     } 
